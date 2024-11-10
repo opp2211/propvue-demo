8779bda9618e611a1adc138fbd8ea8347c668205
Kirill Maltsev <opp.2211@gmail.com>
2024-11-11 04:45:28 +0500

    refactor: delete unnecessary code

--- src/main/java/ru/maltsev/propvuedemo/repository/ProductRepository.java
+++ src/main/java/ru/maltsev/propvuedemo/repository/ProductRepository.java
@@ -1,29 +1,14 @@
 package ru.maltsev.propvuedemo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.maltsev.propvuedemo.model.Product;
 import ru.maltsev.propvuedemo.model.ProductStatus;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, CustomProductRepository {
 
     List<Product> findAllByStatus(ProductStatus status);
-
-    @Query(nativeQuery = true, value = """
-        SELECT SUM(p.value * p.quantity)
-        FROM product p
-        WHERE p.status = :stringStatus
-    """)
-    BigDecimal getTotalValueByStatus(String stringStatus);
-
-    @Query(nativeQuery = true, value = """
-        SELECT SUM(p.value * p.quantity)
-        FROM product p
-    """)
-    BigDecimal getTotalValueOfAll();
 }
